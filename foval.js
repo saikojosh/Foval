250407da04ac40da723d1fcd0ad1c67dc57d0a67
Josh Cole <saikojosh@gmail.com>
2015-01-05 16:33:55 +0000

    Added placeholder foval.js module file

---
+++ foval.js
@@ -0,0 +1,18 @@
+/*
+ * FOVAL.
+ * Wonderfully easy form validation for Node.js.
+ */
+
+var async        = require('async');
+var Countersign  = require('countersign');
+var escapeRegExp = require('escape-regexp');
+var objectAssign = require('object-assign');
+
+/*
+ * Constructor.
+ */
+function Foval (options) {
+
+
+
+};

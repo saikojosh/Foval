6443929c8124c9af36e7ae2a88b7f57fdfad318f
Josh Cole <saikojosh@gmail.com>
2015-10-16 14:59:53 +0100

    Client 0.0.13

--- client/foval-client-jquery.js
+++ client/foval-client-jquery.js
@@ -10,7 +10,7 @@ var FovalClient = {
    * Global properties.
    */
   prop: {
-    version:        '0.0.12',
+    version:        '0.0.13',
     forms:          {},
     defaultTimeout: 10000   //10 seconds
   },

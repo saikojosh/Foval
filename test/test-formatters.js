59a4a858b350e8e30b84bfffbc69000225843710
Josh Cole <saikojosh@gmail.com>
2015-10-18 15:51:33 +0100

    Updated telephone formatting test

--- test/test-formatters.js
+++ test/test-formatters.js
@@ -3,10 +3,32 @@
  */
 
 var Foval  = require('../foval');
-var output = Foval.format('07123456789', 'telephone', {
-  format:        'uk-local',
-  international: false
-});
 
-console.log('output', output);
+// Perform some local tests.
+var localTests = ['+44.7949523641', '07949523641'];
+for (var a = 0, alen = localTests.length ; a < alen ; a++) {
+  var localTest = localTests[a];
 
+  console.log('');
+  console.log(localTest, '>',
+    Foval.format(localTest, 'telephone', {
+      format:        'uk-local',
+      international: false
+    })
+  );
+}
+
+// Perform some international tests.
+var internationalTests = ['+44.7949523641', '07949523641'];
+for (var b = 0, blen = internationalTests.length ; b < blen ; b++) {
+  var internationalTest = internationalTests[b];
+
+  console.log('');
+  console.log(internationalTest, '>',
+    Foval.format(internationalTest, 'telephone', {
+      format:        'uk-local',
+      international: true,
+      countryCode:   '44'
+    })
+  );
+}
